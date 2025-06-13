e89bceedce067114bd53a3e65466a0dd8fc28be8
dipak1920basnet <dipak1920basnet@gmail.com>
2025-06-13 19:38:17 +0545

    adds number of user served

--- resturant.py
+++ resturant.py
@@ -1,7 +1,8 @@
 class Resturant:
-    def __init__(self,resturant,cusine_type):
+    def __init__(self, resturant, cusine_type, number_served = 0):
         self.resturant_name = resturant
         self.cusine_type = cusine_type
+        self.number_served = number_served
 
     def describe_resturant(self):
         print(f"{self.resturant_name} is of {self.cusine_type} type")
@@ -9,15 +10,40 @@ class Resturant:
     def open_resturant(self):
         print(f"This resturant is open")
 
-resturant = Resturant("Java_Coffee","Serves Coffee")
+    def set_number_served(self, number):
+        self.number_served = number
+
+    def increment_number_served(self):
+        self.number_served +=1
+
+
+resturant = Resturant("Java_Coffee", "Serves Coffee")
 print(resturant.resturant_name)
 print(resturant.cusine_type)
-resturant.describe_resturant()
-resturant.open_resturant()
-print()
-print()
-resturant_two = Resturant("Sherpa khaza Ghar","Serves Mongol Food")
-print(resturant_two.resturant_name)
-print(resturant_two.cusine_type)
-resturant_two.describe_resturant()
-resturant_two.open_resturant()
+print(resturant.number_served)
+resturant.addCustomer()
+print(resturant.number_served)
+# resturant.describe_resturant()
+# resturant.open_resturant()
+# print()
+# print()
+# resturant_two = Resturant("Sherpa khaza Ghar", "Serves Mongol Food")
+# print(resturant_two.resturant_name)
+# print(resturant_two.cusine_type)
+# resturant_two.describe_resturant()
+# resturant_two.open_resturant()
+
+
+# class User():
+#     def __init__(self, name, surname, email, phone_number, resturant:Resturant):
+#         self.name = name
+#         self.surname = surname
+#         self.email = email
+#         self.phone_number = phone_number
+#         self.resturant = resturant
+
+#     def describe_user(self):
+#         return f"Hi my name is {self.name} {self.surname} and my email is {self.email} and here is my phone number {self.phone_number}"
+
+#     def greet_user(self):
+#         return f"Hello {self.name} {self.surname} welcome to our resturant {self.resturant.resturant_name}"
